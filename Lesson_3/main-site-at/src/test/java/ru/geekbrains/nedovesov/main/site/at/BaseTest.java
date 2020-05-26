b46ddd3add8b0d685f0639093ceb2387f000f118
NedKonstantin <slavkosnet@mail.ru>
2020-05-27 01:28:00 +0300

    Вроде что-то получилось

---
+++ Lesson_3/main-site-at/src/test/java/ru/geekbrains/nedovesov/main/site/at/BaseTest.java
@@ -0,0 +1,4 @@
+package ru.geekbrains.nedovesov.main.site.at;
+
+public class Base {
+}
---
+++ Lesson_3/main-site-at/src/test/java/ru/geekbrains/nedovesov/main/site/at/NavigationTest.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.nedovesov.main.site.at;
+
+import org.junit.jupiter.api.Test;
+
+public class NavigationTest {
+
+    @Test
+    void navigationPanelTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+
+    }
+
+}
