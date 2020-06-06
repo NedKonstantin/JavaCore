d27d34313a1fb5f3012f6597a7f50c6463e8854e
NedKonstantin <slavkosnet@mail.ru>
2020-06-06 14:00:54 +0300

    Добавлены методы проверки остальных "плашек"

--- Lesson_3/main-site-at/src/main/java/ru/geekbrains/nedovesov/main/site/at/pages/NavigationForSearch.java
+++ Lesson_3/main-site-at/src/main/java/ru/geekbrains/nedovesov/main/site/at/pages/NavigationForSearch.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class NavigationForSearch extends BasePage {
-
     @FindBy (css = "li > a > svg.icon-search")
     private WebElement searchButton;
 
